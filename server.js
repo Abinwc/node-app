fbeabc37c6676c1e49e2b095299e3a5504af3288
abinwc <abinpaul5598@gmail.com>
2021-01-06 12:50:51 +0530

    an html form and get method and printing it from backend

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    console.log(req.query.fname)
+    console.log(req.query.lname)
+  res.send('Hello World!');
+});
+
+const port = 3000 ;
+app.listen(port, function () {
+  console.log('myapp listening on port ' + port);
+});
